52591ed6e7c6870260ab3b97596a82f36e907aaa
ValentinoGaleazzo <164956982+ValentinoGaleazzo@users.noreply.github.com>
2024-06-11 20:15:45 -0300

    Delete Parcial de Algoritmos directory

--- Parcial de Algoritmos/Punto 1.py
+++
@@ -1,9 +0,0 @@
-def lista_inversa(lista):
-    if not lista: 
-        return []
-    else:
-        return lista_inversa(lista[1:]) + [lista[0]]
-    
-# Ej:
-listita = [1, 2, 3, 4, 5]
-print("Lista og", listita, lista_inversa(listita), "y esta es la lista invertida.")
